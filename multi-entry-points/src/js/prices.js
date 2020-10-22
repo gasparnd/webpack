c21606709e33efb84b95529181ec0a0578c8238e
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2020-10-22 09:35:22 -0300

    i create multi entry points

---
+++ multi-entry-points/src/js/prices.js
@@ -0,0 +1,1 @@
+console.log('Hello World from WebPack -  Prices')
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 const path = require('path') //it's help we to create better path's. Package from node.js
 
 module.exports = {
-	entry: './index.js',  //input
-	mode: 'development', //or production
+	entry: path.resolve(__dirname, 'index.js'),  //input
 	output: {
 		path: path.resolve(__dirname, 'dist'),  //return an absolute route where will live the project
-		filename: 'bundle.js'
+		filename: 'bundle.js'  //output file name
 	},
 }
