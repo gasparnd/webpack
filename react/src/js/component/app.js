e20fa113468f9d4b65b66ab28b4f37a5e42e6248
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2020-10-26 18:05:40 -0300

    react app

--- react/src/js/component/app.js
+++ react/src/js/component/app.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import data from './data.json'
+import Loader from './loader'
+console.log(data)
 
 const App = () => {
+	const [loaderList, setLoaderList] = useState([])
+	function handleClick () {
+		setLoaderList(data.loaders)
+	}
 	return (
 		<div>
 			Beutiful React App
+			<ul>
+				{
+					loaderList.map(item => <Loader {...item} key={item.id} />)
+				}	
+			</ul>
+			<button onClick={handleClick}>Show what i learned</button>
 		</div>
 	)
 }
